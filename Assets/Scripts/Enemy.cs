4f83fa7b678834a6c3fefdebbc06a4e28c54f4c0
LiloTM <Laura-U@kabelmail.de>
2024-09-29 02:00:53 +0200

    add animation and enemy model

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int projectileSpeed = 300;
     private GameObject playerHead;
     private EmenyManager eM;
+    private Animator anim;
 
     private void Start()
     {
         playerHead = GameObject.FindGameObjectWithTag("ActorHead");
+        anim = transform.GetChild(0).GetComponent<Animator>();
         InvokeRepeating("Shoot", 3f, 3f);    
     }
     public void getEnemyManager(EmenyManager M)
@@ -21,8 +23,8 @@ public class Enemy : MonoBehaviour
     private void Shoot()
     {
         if (playerHead == null) return;
-
-        GameObject p = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        anim.Play("Attack01");
+        GameObject p = Instantiate(projectilePrefab, transform.GetChild(1).transform.position, Quaternion.identity);
         Rigidbody rb = p.GetComponent<Rigidbody>();
         rb.velocity += (playerHead.transform.position - p.transform.position).normalized * Time.deltaTime * projectileSpeed;
     }
